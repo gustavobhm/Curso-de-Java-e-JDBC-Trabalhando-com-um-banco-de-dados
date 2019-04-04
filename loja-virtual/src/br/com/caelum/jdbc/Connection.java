1264dd28ce746da4297c88c7bc587562adcc9ecb
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-04-04 08:52:14 -0300

    Aula 3 - SQL Injection e Prepared Statement

--- loja-virtual/src/br/com/caelum/jdbc/Connection.java
+++ loja-virtual/src/br/com/caelum/jdbc/Connection.java
@@ -1,7 +1,5 @@
 package br.com.caelum.jdbc;
 
-import org.hsqldb.result.Result;
-
 public interface Connection {
 	Result execute(String sql);
 }
--- loja-virtual/src/br/com/caelum/jdbc/TestaInsercao.java
+++ loja-virtual/src/br/com/caelum/jdbc/TestaInsercao.java
@@ -1,6 +1,7 @@
 package br.com.caelum.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,16 +9,23 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
+		String nome = "Notebook";
+		String descricao = "Notebook i5";
 		Connection connection = Database.getConnection();
-		Statement statement = connection.createStatement();
-		boolean resultado = statement.execute("insert into produto (nome, descricao) values ('Notebook', 'Notebook i5')", 
-				Statement.RETURN_GENERATED_KEYS);
+		
+		String sql = "insert into produto (nome, descricao) values (?, ?)";
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		statement.setString(1, nome);
+		statement.setString(2, descricao);
+		boolean resultado = statement.execute();
 		System.out.println(resultado);
 		ResultSet generatedKeys = statement.getGeneratedKeys();
+		
 		while(generatedKeys.next()) {
 			long id = generatedKeys.getLong("id");
 			System.out.println("id gerado: " + id);
 		}
+		
 		generatedKeys.close();
 		statement.close();
 		connection.close();
